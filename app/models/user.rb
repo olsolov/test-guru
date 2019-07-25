448f7c98a9df2c27cbea9eeb0239dccb8f5b69d1
olgasolov <solo_o_e@mail.ru>
2019-07-25 20:35:23 +0300

    Fix seeds, methods to User, Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def user_tests_by_level(level)
     Test.joins('JOIN results ON tests.id = results.test_id')
-        .where('tests.level = :level AND results.user_id = :user_id', level: level, user_id: id)
+        .where(level: level, results: { user_id: id })
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,51 +6,55 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create([
-                  { title: 'Ruby' },
-                  { title: 'Ruby on Rails' },
-                  { title: 'Git' }
-                ])
-Test.create([
-              { title: 'Основы Ruby', level: 1, category_id: 1 },
-              { title: 'ООП в Ruby', level: 2, category_id: 1 },
-              { title: 'Основы Ruby on Rails', level: 1, category_id: 2 },
-              { title: 'Основы Git', level: 1, category_id: 3 },
-              { title: 'Git branch', level: 2, category_id: 3 }
-            ])
-Question.create([
-                  { body: 'Какой метод позволяет перевести строку в нижний регистр?' },
-                  { body: 'Что такое объект в ООП?' },
-                  { body: 'Как добавить в конфигурацию Rails приложения часовой пояс?' },
-                  { body: 'Что такое Git?' },
-                  { body: 'Какими способами можно слить две ветки?' }
-                ])
-Answer.create([
-                { body: 'dcase()' },
-                { body: 'downcase()', correct: true },
-                { body: 'down()' },
-                { body: 'lowercase()' },
-                { body: 'upcase()' },
-                { body: 'Объект — это сущность, служащая контейнером для данных и управляющая доступом к этим данным.' },
-                { body: 'Объект — это программная модель какого-то реально существующего объекта' },
-                { body: 'Объект — cущность в адресном пространстве вычислительной системы, появляющаяся при создании экземпляра класса' },
-                { body: 'Все ответы верны', correct: true },
-                { body: 'config.time_zone =', correct: true },
-                { body: 'time_zone =' },
-                { body: 'Это облачное хранилище' },
-                { body: 'Это сервер для ваших проектов' },
-                { body: 'Это таск-менеджер' },
-                { body: 'Это распределённая система управления версиями', correct: true },
-                { body: 'git merge' },
-                { body: 'pull request' }
-              ])
-User.create([
-              { name: 'Olga', role: 'admin' },
-              { name: 'New_user', role: 'user' }
-            ])
+categories = Category.create!([
+                                { title: 'Ruby' },
+                                { title: 'Ruby on Rails' },
+                                { title: 'Git' }
+                              ])
 
-Result.create([
-                { user_id: 2, test_id: 1 },
-                { user_id: 2, test_id: 3 },
-                { user_id: 2, test_id: 5 }
-              ])
+tests = Test.create!([
+                       { title: 'Основы Ruby', level: 1, category_id: categories[0].id },
+                       { title: 'ООП в Ruby', level: 2, category_id: categories[0].id },
+                       { title: 'Основы Ruby on Rails', level: 1, category_id: categories[1].id },
+                       { title: 'Основы Git', level: 1, category_id: categories[2].id },
+                       { title: 'Git branch', level: 2, category_id: categories[2].id }
+                     ])
+
+questions = Question.create!([
+                               { body: 'Какой метод позволяет перевести строку в нижний регистр?' },
+                               { body: 'Что такое объект в ООП?' },
+                               { body: 'Как добавить в конфигурацию Rails приложения часовой пояс?' },
+                               { body: 'Что такое Git?' },
+                               { body: 'Какими способами можно слить две ветки?' }
+                             ])
+
+answers = Answer.create!([
+                           { body: 'dcase()' },
+                           { body: 'downcase()', correct: true },
+                           { body: 'down()' },
+                           { body: 'lowercase()' },
+                           { body: 'upcase()' },
+                           { body: 'Объект — это сущность, служащая контейнером для данных и управляющая доступом к этим данным.' },
+                           { body: 'Объект — это программная модель какого-то реально существующего объекта' },
+                           { body: 'Объект — cущность в адресном пространстве вычислительной системы, появляющаяся при создании экземпляра класса' },
+                           { body: 'Все ответы верны', correct: true },
+                           { body: 'config.time_zone =', correct: true },
+                           { body: 'time_zone =' },
+                           { body: 'Это облачное хранилище' },
+                           { body: 'Это сервер для ваших проектов' },
+                           { body: 'Это таск-менеджер' },
+                           { body: 'Это распределённая система управления версиями', correct: true },
+                           { body: 'git merge' },
+                           { body: 'pull request' }
+                         ])
+
+users = User.create!([
+                       { name: 'Olga', role: 'admin' },
+                       { name: 'New_user', role: 'user' }
+                     ])
+
+results = Result.create!([
+                           { user_id: users[1].id, test_id: tests[0].id },
+                           { user_id: users[1].id, test_id: tests[2].id },
+                           { user_id: users[1].id, test_id: tests[4].id }
+                         ])
