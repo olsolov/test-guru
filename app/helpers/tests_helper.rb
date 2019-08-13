6c7d2286d47c9353cd6bd785548dbd4bfa6350d0
olgasolov <solo_o_e@mail.ru>
2019-08-13 12:22:37 +0300

    Add controllers methods, views, helpers

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,15 @@
 module TestsHelper
+  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
+  end
+
+  def test_header(test)
+    if test.new_record?
+      'Create New Test'
+    else
+      "Edit #{test.title} Test"
+    end
+  end
 end
