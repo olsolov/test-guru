538877b28deda342de1a2b0323adb189f70995dc
olgasolov <solo_o_e@mail.ru>
2019-09-18 14:34:55 +0300

    Add dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
-  has_many :gists
+  has_many :gists, dependent: :nullify
 
   devise :database_authenticatable,
          :registerable,
